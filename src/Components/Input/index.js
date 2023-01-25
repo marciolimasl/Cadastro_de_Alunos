0ddcff78a81085f81902f8e111d29e7ac520de7a
marciolimasl <marciolimasl2002@gmail.com>
2023-01-25 20:31:47 -0300

    Add method/Post

--- src/Components/Input/index.js
+++ src/Components/Input/index.js
@@ -1,10 +1,16 @@
 import styles from "./styles.module.css";
 
-const Input = ({ type, name, placeholder }) => {
+const Input = ({ type, name, placeholder, handleChange }) => {
   return (
     <div className={styles.form_control}>
       <label htmlFor={name}>{name}</label>
-      <input type={type} placeholder={placeholder} id={name} name={name} />
+      <input
+        type={type}
+        placeholder={placeholder}
+        id={name}
+        name={name}
+        onChange={handleChange}
+      />
     </div>
   );
 };
--- src/Pages/New Student/index.js
+++ src/Pages/New Student/index.js
@@ -1,23 +1,56 @@
+import { useState } from "react";
 import Input from "../../Components/Input";
 import Submit from "../../Components/Submit";
 import styles from "./styles.module.css";
 
 const NewStudent = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [date, setDate] = useState("");
+
+  const createStudent = (e) => {
+    e.preventDefault();
+    fetch("http://localhost:5000/alunos/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: name,
+        email: email,
+        date: date,
+      }),
+    })
+      .then((resp) => resp.json())
+      .then((date) => {
+        if (date.ok) {
+          alert("Aluno cadastrado com sucesso.");
+        }
+      })
+      .catch((err) => err);
+  };
+
   return (
-    <form className={styles.form}>
+    <form onSubmit={createStudent} className={styles.form}>
       <h1>Cadastrar Aluno</h1>
       <p>Preencha os campos abaixo:</p>
       <Input
         type="text"
         name="Nome:"
         placeholder="Insira o nome do estudante"
+        handleChange={(e) => setName(e.target.value)}
       />
       <Input
         type="text"
         name="Email:"
         placeholder="Insira o email do estudante"
+        handleChange={(e) => setEmail(e.target.value)}
       />
-      <Input type="date" name="Data de Nascimento" />
+      <Input
+        type="date"
+        name="Data de Nascimento"
+        handleChange={(e) => setDate(e.target.value)}
+      />
       <Submit text="Cadastrar" />
     </form>
   );
